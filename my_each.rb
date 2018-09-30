a6f0c859dd710e1d026414e50fcf0b9bf6355fec
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 23:13:49 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
 def my_each(array)
   count = 0
   
-  array.do { |n|
-    while count < array.length
-    count += 1
-    prints n
-    end
-  }
+  if array.length > 0
+    array.do { |n|
+      while count < array.length
+      count += 1
+      prints n
+      end
+      }
+    else
 end
